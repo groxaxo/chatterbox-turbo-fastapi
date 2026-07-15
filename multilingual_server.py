6d83036a60911ad5182e502a415ae39f21bdc184
Vlad Jagodnik <blackstoreonline@gmail.com>
2026-07-15 12:10:02 +1200

    Install Turbo performance runtime in ASGI entrypoint

--- multilingual_server.py
+++ multilingual_server.py
@@ -2,6 +2,8 @@
 
 import server
 from chaturbo_espanol_runtime import install_chaturbo_espanol_runtime
+from performance_runtime import install_turbo_performance_runtime
 
 runtime = install_chaturbo_espanol_runtime(server)
+performance_runtime = install_turbo_performance_runtime(server, runtime)
 app = server.app
